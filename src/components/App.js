94b1fc8cf47d3639432f429e313a0282406f73e2
Rooftrellen <CoderRooftrellen@gmail.com>
2016-07-25 16:48:48 +0800

    add horizon watch.subscribe and store workflow

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,55 @@
 import React from 'react';
 import MessageList from './MessageList';
 
+//include our newly installed horizon client
+const Horizon = require('@horizon/client');
+const horizon = Horizon({ secure: false });
+//this initiates our 'messages' collection inside of our Rethinkdb
+const chat = horizon('messages');
+
 const App = React.createClass({
+  getInitialState(){
+    return {
+      author:false,
+      text: false,
+    }
+  },
+  
+  handleChangeAuthor(event){
+    this.setState({author:event.target.value});
+  },
+  
+  handleChangeText(event){
+    this.setState({text:event.target.value})
+  },
+  
+  sendMessage() {
+    //check for empty strings and return early if a message/author 
+    //isn't entered
+    if(this.state.text === false || this.state.author === false) {
+      alert('Invalid Submission');
+      return;
+    }  
+    let message = {
+      text: this.state.text,
+      author: this.state.author
+    };
+    //the store method will take our new message and store it in our
+    //Rethink collection
+    chat.store(message);
+  },
+  
   render(){
     return (
       <div>
         <form>
         <div className='center'>
-          <button>Send Message</button>
-          <input placeholder='By'></input>
-          <input placeholder='write message here'></input>
+          <button onClick={this.sendMessage}>Send Message</button>
+          <input placeholder='By' onChange={this.handleChangeAuthor}></input>
+          <input placeholder='write message here' onChange={this.handleChangeText}></input>
         </div>
         </form>
-        <MessageList />
+        <MessageList chat={chat}/>
       </div>
      );
   }
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -4,11 +4,24 @@ import Message from './message';
 const MessageList = React.createClass({
   getInitialState(){
     return {
-      data: [{text: 'this is text', author: '@steedhelix'},
+      data: []
+        /*[{text: 'this is text', author: '@steedhelix'},
             {text: 'this is some text', author: '@steedhelix'},
             {text: 'this is more text', author: '@steedhelix'},
-            {text: 'this is other text', author: '@steedhelix'}]
+            {text: 'this is other text', author: '@steedhelix'}]*/
     }
+  },
+  
+  componentDidMount() {
+    this.props.chat.watch().subscribe(
+      (messages) => {
+         let data = messages.map(function(message) {return message});
+         this.setState({data: data});
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   },
 
   render() {
